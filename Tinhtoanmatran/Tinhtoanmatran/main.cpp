2c69d49c33266a8cea858a1a224b3aa461de1f8f
Lê Tuấn Kiệt <kietgolx65234@gmail.com>
2020-02-15 23:58:12 +0700

    thêm tính năng biến đổi ma trận vuông thành ma trận tam giác

--- Tinhtoanmatran/Tinhtoanmatran/main.cpp
+++ Tinhtoanmatran/Tinhtoanmatran/main.cpp
@@ -11,60 +11,98 @@ using namespace std;
 void nhapmatran(int M[max][max], int row, int col);
 void xuatmatran(int M[max][max], int row, int col);
 void congmatran(int A[max][max], int B[max][max], int row, int col);
+void kiemtramatranvuong(int M[max][max],int row,int col);
 void kiemtra2matrancungcap(int row1, int col1, int row2, int col2);
 void trumatran(int A[max][max], int B[max][max], int row, int col);
 void nhanmatran(int A[max][max],int B[max][max],int row1, int col1,int row2,int col2);
+void chuyenthanhmatrantamgiac(int M[max][max],int row);
+void dinhthucmatran(int A[max][max],int row,int col);
 int main(int argc, char const *argv[])
 {
-    int row1, col1, row2, col2;
+    int row1, col1, row2, col2,n;
     int A[max][max], B[max][max];
-    std::cout << "Nhập số dòng i của ma trận 1 = "; std::cin >> row1;
-    std::cout << "Nhập số cột j ma trận 1 = "; std::cin >> col1;
+    cout<<"Nhập số ma trận muốn tạo : ";cin>>n;
+    if (n==1) {
+        int biendoi;
+        cout << "Nhập số dòng i của ma trận = "; cin >> row1;
+        cout << "Nhập số cột j ma trận = "; cin >> col1;
+        nhapmatran(A, row1, col1);
+        cout << "----Mời bạn chọn biểu thức biến đổi----"<<'\n';
+        cout<< "Biến thành ma trận tam giác (1)\nTính định thức ma trận (2)";cin>>biendoi;
+        switch (biendoi) {
+            case 1:
+                kiemtramatranvuong(A, row1, col1);
+                chuyenthanhmatrantamgiac(A, row1);
+                xuatmatran(A, row1, row1);
+                break;
+            case 2:
+                dinhthucmatran(A, row1, col1);
+                break;
+            default:
+                cout<<"Bạn đã nhập sai phép biến đổi, mời bạn chạy lại chương trình";
+                break;
+        }
+    }
+    else if(n==2){
+    cout << "Nhập số dòng i của ma trận 1 = "; cin >> row1;
+    cout << "Nhập số cột j ma trận 1 = "; cin >> col1;
     nhapmatran(A, row1, col1);
-    std::cout << "Nhập số dòng i ma trận 2 = "; std::cin >> row2;
-    std::cout << "Nhập số cột j ma trận 2 = "; std::cin >> col2;
+    cout << "Nhập số dòng i ma trận 2 = "; cin >> row2;
+    cout << "Nhập số cột j ma trận 2 = "; cin >> col2;
     nhapmatran(B, row2, col2);
-    std::cout << "-----Mời bạn chọn phép tính bằng số tương ứng-----" << '\n';
+    cout << "-----Mời bạn chọn phép tính bằng số tương ứng-----" << '\n';
     int pheptinh;
-    std::cout << "Phép cộng (1)\nPhép trừ (2)\nPhép nhân(3)\nPhép chia (4)\n Chọn số : "; std::cin >> pheptinh;
-    if (pheptinh == 1){
-        kiemtra2matrancungcap(row1, col1, row2, col2);
-        congmatran(A, B, row1, col1);
-    }
-    else if(pheptinh == 2){
-        kiemtra2matrancungcap(row1, col1, row2, col2);
-        trumatran(A, B, row1, col1);
-    }
-    else if (pheptinh==3)
-        nhanmatran(A,B,row1,col1,row2,col2);
+    cout << "Phép cộng (1)\nPhép trừ (2)\nPhép nhân(3)\nPhép chia (4)\n Chọn số : "; cin >> pheptinh;
+        switch (pheptinh) {
+            case 1:
+                kiemtra2matrancungcap(row1, col1, row2, col2);
+                congmatran(A, B, row1, col1);
+                break;
+            case 2:
+                kiemtra2matrancungcap(row1, col1, row2, col2);
+                trumatran(A, B, row1, col1);
+                break;
+            case 3:
+                nhanmatran(A,B,row1,col1,row2,col2);
+                break;
+            case 4:
+                break;
+            default:
+                cout<<"Bạn đã nhập sai phép tính, mời chạy lại chương trình nhập lại";
+                break;
+        }
     return 0;
+  }
 }
-
-void nhapmatran(int M[max][max], int row, int col)
-{
+void nhapmatran(int M[max][max], int row, int col){
     for (int i = 1; i <= row; i++) {
         for (int j = 1; j <= col; j++) {
-            std::cout << "M[" << i << "][" << j << "]= ";
-            std::cin >> M[i][j];
+            cout << "M[" << i << "][" << j << "]= ";
+            cin >> M[i][j];
         }
-        std::cout << '\n';
+        cout << '\n';
     }
 }
 
 void xuatmatran(int M[max][max], int row, int col)
 {
-    std::cout << "----Xuất Ma trận---" << '\n';
+    cout << "----Xuất Ma trận---" << '\n';
     for (int i = 1; i <= row; i++) {
         for (int j = 1; j <= col; j++) {
-            std::cout << M[i][j] << '\t';
+            cout << M[i][j] << '\t';
         }
-        std::cout << '\n';
+        cout << '\n';
     }
 }
-
+void kiemtramatranvuong(int M[max][max],int row,int col){
+    if (row!=col) {
+        cout<<"Lỗi kích thước, ma trận phải cùng cấp";
+        exit(0);
+    }
+}
 void kiemtra2matrancungcap(int row1, int col1, int row2, int col2){
     if (row1 != row2 || col1 != col2) {
-        std::cout << "Lỗi kích thước không thực hiện phép tính được" << '\n';
+        cout << "Lỗi kích thước không thực hiện phép tính được" << '\n';
         exit(0);
     }
 }
@@ -92,7 +130,7 @@ void trumatran(int A[max][max], int B[max][max], int row, int col)
 void nhanmatran(int A[max][max],int B[max][max],int row1, int col1,int row2,int col2){
     int C[max][max];
     if (col1!=row2) {
-        std::cout<<"Muốn nhân 2 ma trận bắt buộc cột của ma trận 1 phải bằng dòng của ma trận 2";
+        cout<<"Muốn nhân 2 ma trận bắt buộc cột của ma trận 1 phải bằng dòng của ma trận 2";
         exit(0);
     }
     else{
@@ -106,4 +144,22 @@ void nhanmatran(int A[max][max],int B[max][max],int row1, int col1,int row2,int 
         }
     }
     xuatmatran(C, row1, col2);
+}
+void chuyenthanhmatrantamgiac(int M[max][max],int row){
+    float temp;
+    for(int i=1; i<row; i++){
+        for(int j=i+1; j<row+1; j++){
+            temp=M[j][i]/M[i][i];
+            for(int k=i; k<row+1; k++){
+                M[j][k]-=temp*M[i][k];
+            }
+        }
+    }
+}
+void dinhthucmatran(int M[max][max],int row,int col){
+    chuyenthanhmatrantamgiac(M, row);
+    double temp = 1;
+    for(int i = 0; i<row; i++)
+      temp*=M[i][i];
+    cout<<"Định thức ma trận = "<<temp;
 }
